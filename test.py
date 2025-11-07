a55f620ee31a607320d23d67d21bccc258f4c531
Etsku520 <eetu.huovila@gmail.com>
2025-11-07 12:42:24 +0200

    Added findExistingConnectionPairs to return connections use in the input, as unique pairs.

--- test.py
+++ test.py
@@ -11,15 +11,30 @@ def makeConnectionMatrix(connections):
 		conMatrix[i, j] = 1
 	return conMatrix
 
-def removeIllegal(parallelGates, connections):
-	conMatrix = makeConnectionMatrix(connections=connections)
+def findConnections(gates):
+	sumMatrix = gates + gates.T
+	sumIsZero = sumMatrix == 0
+	notZeroGate = (gates != 0)
+	connections = notZeroGate.logical_and(sumIsZero)
 
-	s2 = parallelGates + parallelGates.T
-	d = s2 == 0
-	c2 = (parallelGates != 0)
-	d2 = d.logical_and(c2)
-	illegalConnections = d2.logical_and(conMatrix == 0)
-	legalConnections = d2.logical_and(conMatrix == 1)
+	return connections
+
+def findExistingConnectionPairs(gates, unique=True):
+	cons = findConnections(gates)
+	conList = cons.nonzero()
+	conPairs = torch.tensor([sorted(p) for p in conList])
+	if unique:
+		conPairs = conPairs.unique(dim=0)
+
+	return conPairs
+
+
+def removeIllegal(parallelGates, possibleConnections):
+	conMatrix = makeConnectionMatrix(connections=possibleConnections)
+
+	parallelConnections = findConnections(parallelGates)
+	illegalConnections = parallelConnections.logical_and(conMatrix == 0)
+	legalConnections = parallelConnections.logical_and(conMatrix == 1)
 
 	hasIllegal = torch.any(illegalConnections, dim=1)
 	hasLegal = torch.any(legalConnections, dim=1)
@@ -27,7 +42,7 @@ def removeIllegal(parallelGates, connections):
 
 	return allIllegal
 
-if __name__ == "__main__":
+def removeIllegalTest():
 	connections = torch.tensor(
 		[(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8)]
 	)
@@ -36,3 +51,12 @@ if __name__ == "__main__":
 
 	print(gates)
 	print(r)
+
+def existingPairsTest():
+	gates = torch.tensor([[1, 2, 3, -1, 4, 2, -2, 0, 0]])
+	print(gates.nonzero(as_tuple=True))
+	conPairs = findExistingConnectionPairs(gates)
+	print(conPairs)
+
+if __name__ == "__main__":
+	existingPairsTest()
